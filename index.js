b8317d267f8a8b227c3a10cd70d29c797d472b3d
Linus <s3717624@student.rmit.edu.au>
2021-01-22 14:52:19 +1100

    added all labs

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const Discord = require("discord.js");
 const puppeteer = require("puppeteer");
 const {prefix,  w, h} = require("./config.json");
 const client = new Discord.Client();
+const mdapi = require("mangadex-full-api");
+mdapi.agent.domainOverride = "https://mangadex.org/";
 
 //on client successful build
 client.on("ready", () => { 
     console.log(`${client.user.username} is online`),
-    client.user.setPresence({ status: 'online', game: { name: 'Need help? | #help' } });   ;
+    client.user.setPresence({ status: 'online', game: { name: 'with DN' } });   
+    /* client.user.setPresence({ status: 'online', game: { name: 'Need help? | #help' } });   ; */
 });
 
 //set date
@@ -36,7 +39,7 @@ const getDate = async () => {
     });
 })();
 
-//scraping function
+//scraping function for uber
 const scrapeUber = async () => {
     const page = await browser.newPage();
     await page.setJavaScriptEnabled(false);
@@ -62,18 +65,116 @@ const scrapeUber = async () => {
 
 }
 
+//scraping function for merc
+const scrapeMerc = async () => {
+    const page = await browser.newPage();
+    await page.setJavaScriptEnabled(false);
+    await page.setViewport({
+        width: w,
+        height: h
+    });
+
+    await page.goto('https://www.poelab.com/qxioz');
+    await page.setViewport({
+    width: w,
+    height: h
+    });
+
+    await page.waitForSelector('#notesImg');
+    const getImgSrc = await page.$eval('#notesImg', img => img.getAttribute('src'));
+    console.log(getImgSrc);
+
+    //const png = await page.$('#notesImg');
+    //const screenshot = await png.screenshot({type: 'png'});
+
+    return getImgSrc;
+
+}
+
+//scraping function for cruel
+const scrapeCruel = async () => {
+    const page = await browser.newPage();
+    await page.setJavaScriptEnabled(false);
+    await page.setViewport({
+        width: w,
+        height: h
+    });
+
+    await page.goto('https://www.poelab.com/bdndp');
+    await page.setViewport({
+    width: w,
+    height: h
+    });
+
+    await page.waitForSelector('#notesImg');
+    const getImgSrc = await page.$eval('#notesImg', img => img.getAttribute('src'));
+    console.log(getImgSrc);
+
+    //const png = await page.$('#notesImg');
+    //const screenshot = await png.screenshot({type: 'png'});
+
+    return getImgSrc;
+
+}
+
+//scraping function for normal
+const scrapeNormal = async () => {
+    const page = await browser.newPage();
+    await page.setJavaScriptEnabled(false);
+    await page.setViewport({
+        width: w,
+        height: h
+    });
+
+    await page.goto('https://www.poelab.com/nidbr');
+    await page.setViewport({
+    width: w,
+    height: h
+    });
+
+    await page.waitForSelector('#notesImg');
+    const getImgSrc = await page.$eval('#notesImg', img => img.getAttribute('src'));
+    console.log(getImgSrc);
+
+    //const png = await page.$('#notesImg');
+    //const screenshot = await png.screenshot({type: 'png'});
+
+    return getImgSrc;
+
+}
+
 client.on("message", async message => {
     try {
         if (message.content.startsWith(`${prefix}uber`)) {
             var date = await getDate();
             const values = await scrapeUber();
-            message.channel.send(`Here is todays uber for ${date}\n${values} `);
+            message.channel.send(`Here is todays uber lab for ${date}\n${values} `);
+        }
+        else if (message.content.startsWith(`${prefix}merc`)) {
+            var date = await getDate();
+            const values = await scrapeMerc();
+            message.channel.send(`Here is todays merc lab for ${date}\n${values} `);
+        }
+        else if (message.content.startsWith(`${prefix}cruel`)) {
+            var date = await getDate();
+            const values = await scrapeCruel();
+            message.channel.send(`Here is todays cruel lab for ${date}\n${values} `);
+        }
+        else if (message.content.startsWith(`${prefix}normal`)) {
+            var date = await getDate();
+            const values = await scrapeNormal();
+            message.channel.send(`Here is todays uber lab for ${date}\n${values} `);
         }
     }catch (err){
     console.error("Error has occured")
     }
 
 })
+
+
+
+
+
 
 client.on("message", async message => {
     try {
@@ -86,4 +187,10 @@ client.on("message", async message => {
 
 })
 
+
+
+
+
+
 client.login(process.env.BOT_TOKEN); 
+
